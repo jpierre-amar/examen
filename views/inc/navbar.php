be30cf5df3ab0c61420bc807102ca5f48b71e974
JpierreAMAR <amarjp13@gmail.com>
2025-01-06 16:23:24 +0100

    Maj

--- views/inc/navbar.php
+++ views/inc/navbar.php
@@ -2,17 +2,14 @@
 // Menu de navigation réutilisable pour l'application
 ?>
 <nav class="navbar">
-        <a href="/index.php">Accueil</a>
+        <a href="index.php?ctrl=Home&action=index">Accueil</a>
         <a href="/babies">Mes bébés</a>
         <a href="/activities">Activités</a>
         <a href="/statistics">Statistiques</a>
         <a href="/settings">Paramètres</a>
-    <?php
-    if (!isset($_SESSION['user'])) {
-        <a href="index.php?ctrl=Auth&action=showLoginForm">Se connecter</a>
-    }else {
+    <?php if (!isset($_SESSION['user'])): ?>
+        <a href="index.php?ctrl=Auth&action=showLoginForm">Se connecter / S'inscrire</a>
+    <?php else: ?>
         <a href="index.php?ctrl=Auth&action=logout">Se déconnecter</a>
-    }
-    ?>
-
+    <?php endif; ?>
 </nav>
