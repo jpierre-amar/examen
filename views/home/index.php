87cf1a1b3b797f07b4193264ea2e6d833733706a
JpierreAMAR <amarjp13@gmail.com>
2025-01-08 16:52:18 +0100

    Maj

--- views/home/index.php
+++ views/home/index.php
@@ -1,6 +1,10 @@
 <main class="bg-gray-100 flex items-center justify-center mt-12 mb-20">
 <div class="bg-white shadow-lg rounded-lg p-8 max-w-md text-center">
-<h1 class="text-3xl font-bold text-center my-6">Bienvenue sur Baby Tracker</h1>
+    <h1 class="text-3xl font-bold text-center my-6">Bienvenue sur Baby Tracker</h1>
+    <p class="text-center text-lg text-gray-700 mt-4 mb-12">
+        Baby Tracker est votre compagnon idéal pour suivre les activités, les progrès et les besoins de vos enfants.
+        Simplifiez votre quotidien en enregistrant les repas, les siestes, les changements de couches, et bien plus qui arrivent prochainement !
+    </p>
 <?php if (isset($_SESSION['user_id'])): ?>
     <p class="text-center text-lg">Vous êtes connecté</p>
     <a href="index.php?ctrl=Baby&action=index" class="text-blue-500 hover:text-blue-700 underline">Voir mes bébés</a>
@@ -13,6 +17,6 @@
         </a>
     </div>
 <?php endif; ?>
-    <?php dump($_SESSION, "SESSION"); ?>
+<!--    --><?php //dump($_SESSION, "SESSION"); ?>
 </div>
 </main>
--- views/inc/navbar.php
+++ views/inc/navbar.php
@@ -13,10 +13,10 @@
 
             <!-- Liens de navigation -->
             <div class="hidden md:flex flex-1 items-center" id="navbar-links">
-                <div class="flex flex-1 space-x-4">
+                <div class="flex flex-1 space-x-4 ml-10">
                     <a href="index.php?ctrl=Home&action=index" class="block px-2 py-1 hover:text-gray-300">Accueil</a>
                     <a href="index.php?ctrl=Baby&action=index" class="block px-2 py-1 hover:text-gray-300">Mes bébés</a>
-                    <a href="/statistics" class="block px-2 py-1 hover:text-gray-300">Statistiques</a>
+                   <!-- <a href="/statistics" class="block px-2 py-1 hover:text-gray-300">Statistiques</a> -->
                     <a href="/settings" class="block px-2 py-1 hover:text-gray-300">Paramètres</a>
                 </div>
 
